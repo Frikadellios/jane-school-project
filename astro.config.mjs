e17e66c4cd9f77cefce3302c7833e0b9980925ab
Frikadellios <devopsick@pm.me>
2024-09-25 14:46:23 +0300

    add

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,8 +15,7 @@ export default defineConfig({
   site: 'https://jane-school.pages.dev',
   vite: {
     build: {
-      chunkSizeWarningLimit: 2000,
-      cssMinify: 'lightningcss'
+      chunkSizeWarningLimit: 2000
     },
     plugins: [tailwindcss()],
     define: {
